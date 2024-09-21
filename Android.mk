97a0f67a17d19db258abfd68efb1cded57ef7c2f
Ezleak <116900642+Ezleak@users.noreply.github.com>
2024-09-21 12:23:46 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -89,17 +89,17 @@ LOCAL_SRC_FILES := \
 	src/glx/gbm.c \
 	src/glx/streaming.c \
 
-LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -O3 -fvisibility=hidden -include include/android_debug.h
+LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -Ofast -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 LOCAL_CFLAGS += -DDEBUG
 LOCAL_CFLAGS += -DUSE_ANDROID_LOG
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
-LOCAL_CFLAGS += -DDEFAULT_ES=2
+LOCAL_CFLAGS += -DDEFAULT_ES=3
 //TODO: maybe temporary?
 LOCAL_CFLAGS += -Wno-typedef-redefinition -Wno-dangling-else
 
-LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS := -llog -landroid -lGLESv3
 #building as a static lib
 
 include $(BUILD_SHARED_LIBRARY)
